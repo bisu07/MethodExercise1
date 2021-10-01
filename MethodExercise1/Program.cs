b990e61025049b1718060c3af122c53a844c7a95
bisu07 <g26.bishal@gmail.com>
2021-10-01 00:35:48 -0500

    this is initial commit

---
+++ MethodExercise1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace MethodExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise1();
+        }
+        public static void Exercise1()
+        {
+            Console.WriteLine("Enter Your Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your favourite color: ");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("Which your favourite animal: ");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("Enter your best musical band: ");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"My name is {name},i like {color} tshirt mostly,i love {animal} and kept at my home.I usually listen my best {band} music.");
+        }
+    }
+
+    
+
+
+}
